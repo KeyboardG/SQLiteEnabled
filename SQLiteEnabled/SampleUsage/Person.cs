1a8131f73263fbd3cf8d5858854279ca4e8b95b9
Gregory Brooks <Dark_Halmut@hotmail.com>
2013-04-20 00:20:49 -0400

    Added sample usage code.

---
+++ SQLiteEnabled/SampleUsage/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteEnabled.SampleUsage
+{
+    class Person :SQLiteEnabled
+    {
+        // Define additional fields particular to this type of object.
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        private Int64 age;  // Note that Int64 is required to map to a 64-bit SQLite Int.
+        public Int64 Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+
+        // SQLite Functions ------------------------------
+
+        /// <summary>
+        /// Function simply converts a dynamic of a Person object into a strongly typed version.
+        /// NOTE: Look into building this into the SQLiteEnabled base class using the Activator.CreateInstance().
+        /// </summary>
+        /// <param name="dynamicPerson">A person object retreived from the SQLite database</param>
+        /// <returns>Strongly typed version of the Person object</returns>
+        public static Person FromDynamicConverter(dynamic dynamicPerson)
+        {
+            Person Returned = new Person();
+            Returned.FirstName = dynamicPerson.FirstName;
+            Returned.LastName = dynamicPerson.LastName;
+            Returned.Age = dynamicPerson.Age;
+            
+            return Returned;
+        }
+    }
+}
---
+++ SQLiteEnabled/SampleUsage/SampleUsage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+using SQLiteEnabled;
+
+
+namespace SQLiteEnabled.SampleUsage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Connect or create the SQLite database.
+            var SQLiteConnection = new SQLiteConnection("Data Source=Sample.db;New=True;Version=3;Max Pool Size=100;");
+            SQLiteConnection.Open();
+
+            // Create the table if it doesn't exist.
+            if (!SQLiteEnabled.DoesSQLiteTableExist(SQLiteConnection, typeof(Person)))
+            {
+                Console.WriteLine(SQLiteEnabled.CreateSQLiteTable(SQLiteConnection, typeof(Person)));
+            }
+
+            // Retreive any existing people.
+            // See .ConvertAll<> is what converts from the dynamic to the strongly typed object.
+            List<Person> People = SQLiteEnabled.RetreiveFromDataBase(SQLiteConnection, typeof(Person)).ConvertAll<Person>(p => Person.FromDynamicConverter(p));
+                
+            // Creat a bunch of random new people.
+            for (int i = 0; i < 1000; i++)
+            {
+                var NewGuy = new Person();
+                NewGuy.FirstName = "John";
+                NewGuy.LastName = "Doe";
+                NewGuy.Age = i;
+                People.Add(NewGuy);
+            }
+
+            // Commit the changes. 
+            // Still a little ugly glue to convert from the strongly typed list to dynamic.
+            // What I don't like about dynamic in .Net 4.5 is that you can pass a strongly typed object to a function's dynamic parameter,
+            // but you cannot pass a strongly typed list into a functions list of dynamic parameter.
+            SQLiteEnabled.CommitData(SQLiteConnection, People.ConvertAll<dynamic>(p => p), typeof(Person));
+
+            Console.WriteLine("Derp");
+            Console.ReadKey();
+        }
+    }
+}
